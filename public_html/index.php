e044093cead207c06420d60f9b4352d5331346ce
Marko Perica <marko.perica2@gmail.com>
2022-09-27 08:39:04 +0200

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File converter</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="drop-zone">
+        <span class="drop-zone__prompt">Drop file here or click to upload</span>
+        <input type="file" name="myFile" class="drop-zone__input">
+    </div>
+
+
+    <script src="main.js"></script>
+</body>
+</html>
